9f7c357864390cd361afe8b96519fa8751c55bc4
JonathanGeoffroy <geoffroy.jonathan@gmail.com>
2013-06-25 22:36:31 +0200

    create Riding interface & create a rider humain

---
+++ AndroidStrategic/src/jonathan/geoffroy/androidstrategic/model/fighters/Cavalier.java
@@ -0,0 +1,10 @@
+package jonathan.geoffroy.androidstrategic.model.fighters;
+
+public class Cavalier extends Human implements Riding {
+
+	@Override
+	protected String defaultName() {
+		return "Cavalier";
+	}
+
+}
---
+++ AndroidStrategic/src/jonathan/geoffroy/androidstrategic/model/fighters/Riding.java
@@ -0,0 +1,5 @@
+package jonathan.geoffroy.androidstrategic.model.fighters;
+
+public interface Riding {
+
+}
